d1a73f95d0bbe72f48362bd9b8e4ebff4af31ef5
Othmane Mahfoud <mahfoud.othmane97@gmail.com>
2024-04-05 02:19:59 +0200

    persisting messages across requests

--- chatbot.py
+++ chatbot.py
@@ -8,18 +8,20 @@ api_key = config["OPENAI_API_KEY"]
 
 client = OpenAI(api_key = api_key)
 
+messages = []
+
 while True:
     try:
         user_input = input("You: ")
+        messages.append({"role": "user", "content": user_input})
         res = client.chat.completions.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": user_input}
-            ]
+            messages=messages
         )
-        print(res.choices[0].message.content)
+        reply = {"role": "assistant", "content": res.choices[0].message.content}
+        messages.append(reply)
+        print("Assistant: " + res.choices[0].message.content)
+
     except KeyboardInterrupt:
         print("End of conversation .. Goodbye!")
         break
-
-print(res)
